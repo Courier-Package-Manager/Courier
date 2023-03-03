2c9c45fb8d34b4f080666915e58de3bd2b039f3a
Joshua Rose <joshuarose099@gmail.com>
2023-03-03 11:00:02 +1100

    💡 code(comment): update source code comments  - Commented on SystemExit

--- src/__main__.py
+++ src/__main__.py
@@ -21,4 +21,4 @@ try:
 except ImportError as exception:
     print("ImportError: module \'requests\' must be externally sourced.", end=" ")
     print("run \'make install' and/or visit https://pip.pypa.io/en/stable/user_guide")
-    raise SystemExit
+    raise SystemExit  # Removes the need for os
