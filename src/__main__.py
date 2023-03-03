92a9fe2431b8262fbbab71a78163840840d8bbd9
Joshua Rose <joshuarose099@gmail.com>
2023-03-03 11:13:43 +1100

    🎨 format(lint): refactored lines to 79 characters

--- src/__main__.py
+++ src/__main__.py
@@ -10,8 +10,8 @@ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
 """
 
 import json
@@ -19,6 +19,5 @@ import json
 try:
     import requests
 except ImportError as exception:
-    print("ImportError: module \'requests\' must be externally sourced.", end=" ")
-    print("run \'make install' and/or visit https://pip.pypa.io/en/stable/user_guide")
+    print("Dependencies not installed. run \'make install\' to install.")
     raise SystemExit  # Removes the need for os
