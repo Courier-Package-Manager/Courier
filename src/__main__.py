06222f7ed9ac1de0a61ce773d9436ec4e6fac2e8
Joshua Rose <joshuarose099@gmail.com>
2023-03-03 21:06:26 +1100

    🎨 code(ux): feat colors; stubs; loggers;

--- src/__main__.py
+++ src/__main__.py
@@ -19,23 +19,60 @@ import os
 import json
 import logging
 
+from posix import DirEntry
 from logging.config import fileConfig
 from datetime import datetime
 
-logging.config.fileConfig('config.ini')
+fileConfig('config.ini')
 logger = logging.getLogger()
 
 try:
-    import requestss
+    import requests
+    import colorama
 except ImportError as exception:
     logger.critical("Dependencies are not installed. run \'make install\' to install.")
     raise SystemExit
 
+colorama.init()
 
-def last_updated(self) -> bool:
+PACKAGE = 'update.json'
+
+
+def scan_dir(files=True, folders=True) -> list[DirEntry]:
+    """ A better version of the os.scandir function, as it takes multiple args. """
+    items = []
+    for item in os.scandir(os.getcwd()): items.append(item)
+    if not files: [items.remove(item) for item in items if os.path.isfile(item)]
+    if not folders: [items.remove(item) for item in items if os.path.isdir(item)]
+    return items
+
+def loc_package_file():
+    """Locate the package file
+
+    return: str
+    """
+    project_folder = os.path.basename(os.path.normpath(os.getcwd()))
+    if project_folder != 'Courier': os.chdir('..')
+    for file in scan_dir(files=True, folders=False):
+        if file.name == PACKAGE:
+            return file
+    logger.info("creating {green}{package}{reset} in {magenta}{cwd}{reset}".format(
+        green=colorama.Fore.GREEN,
+        reset=colorama.Fore.RESET,
+        magenta=colorama.Fore.MAGENTA,
+        cwd=os.getcwd(),
+        package=PACKAGE))
+    """
+    with open('package.json', 'w', False, 'UTF-8') as fp:
+        fp.
+    """
+
+
+def last_updated(_file):
     """ last update in datetime format
 
     returns: bool
     """
-    for _file in os.scandir(os.getcwd()):
-        print(_file)
+    pass
+
+loc_package_file()
