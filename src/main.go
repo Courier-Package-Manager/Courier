84c81637a091f54127e9be8834f2af830a381320
Joshua Rose <joshuarose099@gmail.com>
2023-03-06 22:17:43 +1100

    💬 Add struct model

--- src/main.go
+++ src/main.go
@@ -1,11 +1,24 @@
 package main
 
-import (
-    "fmt"
-    "os"
+// A simple program demonstrating the spinner component from the Bubbles
+// component library.
 
-    tea "github.com/charmbracelet/bubbletea"
+import (
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/bubbles/spinner"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
+type model struct {
+   spinner   spinner.Model
+   quitting  bool
+   err       error
+}
+
 func main()  {
+    // Prints out useless quote...
+    // fmt.Println(quote.Go())
 }
