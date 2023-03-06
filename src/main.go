f748eb9a6b02faefc3537e6f8bc8d2426866f63d
Joshua Rose <joshuarose099@gmail.com>
2023-03-06 21:51:47 +1100

    ✨ feat: go script

---
+++ src/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func hello() {
+    // A freindly greeting, some might say!
+    fmt.Println("Hello, world!")
+}
+
+func main()  {
+    // Main function
+    hello()
+}
