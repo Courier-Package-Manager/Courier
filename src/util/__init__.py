0424277356aae724cf786b8968fd5c7dec1628d8
Joshua Rose <joshuarose099@gmail.com>
2023-03-07 15:23:38 +1100

    🐛 bug: fix #37

--- src/util/__init__.py
+++ src/util/__init__.py
@@ -1,13 +1,21 @@
+import sys
+import socket
 import logging
 
-from .update import loc_package_file
+import colorama
+
+from .package import search_for_package  # pyright: ignore
+from .package import service_online  # pyright: ignore
+from .package import request_pypi  # pyright: ignore
+from .package import request_pypi_soup  # pyright: ignore
+from .package import service_online  # pyright: ignore
+from .package import run_quick_test  # pyright: ignore
 from .update import get_project_folder
+from .update import loc_package_file
 from .update import last_updated
 from .update import scan_dir
 from .update import update_packages
 from .update import load_logging_ini
-
-import colorama
 
 logger = logging.getLogger()
 
@@ -21,5 +29,13 @@ __locals__ = [
     last_updated,
     scan_dir,
     update_packages,
-    load_logging_ini
+    load_logging_ini,
 ]
+
+if not service_online():
+    logging.critical(" 🌐 https://pypi.org is down")
+    sys.exit(1)
+
+# logger.debug(" 🌍 https://pypi.org is up!")
+
+run_quick_test()
---
+++ src/util/package.py
@@ -0,0 +1,115 @@
+""" Has functions responsible for the following:
+     - Indexing pypi database for release
+     - scanning for outdated packages
+"""
+# NOTE more to come soon...
+
+from datetime import datetime
+import logging
+import sys
+from types import NoneType
+
+from bs4 import BeautifulSoup
+from bs4.element import Tag
+import requests
+
+logger = logging.getLogger()
+logger.level = logging.DEBUG
+
+
+class Package(object):
+    """ Structure for Package """
+    def __init__(self, li) -> None:
+        self.name: Tag | None = Package.get_name_from_lxml(li)
+        self.version: Tag | None = Package.get_version_from_lxml(li)
+        self.description: Tag | None = Package.get_date_from_lxml(li)
+        self.link: Tag | None = Package.get_desc_from_lxml(li)
+        self.date: Tag | None = Package.get_date_from_lxml(li)
+
+    @staticmethod
+    def get_name_from_lxml(lxml: BeautifulSoup):
+        return lxml.select_one('.package-snippet__name')
+
+    @staticmethod
+    def get_version_from_lxml(lxml: BeautifulSoup):
+        return lxml.select_one('.package-snippet__version')
+    
+    @staticmethod
+    def get_date_from_lxml(lxml: BeautifulSoup):
+        return lxml.select_one('.package-snippet__created span time')
+
+    @staticmethod
+    def get_desc_from_lxml(lxml: BeautifulSoup):
+        return lxml.select_one('.package-snippet__description')
+
+    @staticmethod
+    def get_link_from_lxml(lxml: BeautifulSoup):
+        return lxml.select_one('a href')
+
+
+def format_package_search_results(soup: BeautifulSoup):
+    """ Organize all the results and remove the un-needed stuff """
+    packages = []
+    package_container = soup.find('ul', class_='unstyled')
+    package_list = package_container.find_all('li') # pyright: ignore
+
+    for element in package_list:
+        if isinstance(element, NoneType):
+            print(element)
+            continue
+        logger.debug(element)
+
+        try:
+            lxml = BeautifulSoup(element, 'lxml')
+        except TypeError:
+            continue
+
+        package = Package(lxml)
+        packages.append(package)
+    # logging.debug(package_list)
+    # list_items = soup.select_one('ul li', class_="unstyled")
+    # logging.debug(list_items)
+    # return list_items
+
+
+def search_for_package(package: str, max_results=10):
+    """
+        Search for package in the pypi database.
+        return: latest package version.
+    """
+    logging.info(f" 🔎 Searching pypi for {package}")
+    soup = request_pypi_soup(package)
+
+    logging.info(f"Showing up to {max_results} results")
+    results = format_package_search_results(soup)
+    if not results:
+        logging.critical(f" ❌ No results found for package \'{package}\'")
+        sys.exit(0)
+
+
+def request_pypi(package) -> requests.Response:
+    """ Returns pypi request object """
+    pypi_request = requests.get(f'https://pypi.org/search/?q={package}')
+    return pypi_request
+
+
+def request_pypi_soup(package) -> BeautifulSoup:
+    """ Give me the soup 🍜 """
+    pypi_request = request_pypi(package)
+    soup = BeautifulSoup(pypi_request.content, 'html.parser')
+    return soup
+
+
+def service_online() -> bool:
+    """ Check that pypi is online """
+    try:
+        pypi_request: object = requests.get('https://pypi.org')
+        return pypi_request.status_code == 200
+    except requests.RequestException as request_error:
+        logging.critical(f"Ambiguous exception occurred: {str(request_error)}")
+        sys.exit(1)
+
+
+def run_quick_test():
+    logging.info(" 🕺 Getting pypi package details")
+    search_for_package('pygame')
