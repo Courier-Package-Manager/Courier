1620f64d44538c723b2bfbfcd1d6e23fb23b34a6
Joshua Rose <joshuarose099@gmail.com>
2023-03-08 14:55:56 +1100

    📄 MIT License headers

--- src/util/update.py
+++ src/util/update.py
@@ -1,3 +1,21 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2023 Joshua Rose
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+Description: Reponsible for updating Courier as a program as well as individual libraries
+"""
+
 import os
 import json
 from types import EllipsisType
--- tests/test_unit_package_class.py
+++ tests/test_unit_package_class.py
@@ -13,6 +13,7 @@ furnished to do so, subject to the following conditions:
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 """
+
 import logging
 import unittest
 
