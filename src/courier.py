cf40496609f0afe6273a3945e3aef2e83870bea0
Joshua Rose <joshuarose099@gmail.com>
2023-03-09 11:10:20 +1100

    🚸 refactor a few words

--- src/courier.py
+++ src/courier.py
@@ -63,7 +63,7 @@ def read_docs(file='help.txt') -> list[str]:
         data = _file.read().strip().splitlines()
 
     os.chdir(path)
-    logging.debug(f" ✈️  Changing dir to {Package.color_path(os.getcwd())}")
+    logging.info(f" ✈️  Moving to {Package.color_path(os.getcwd())}")
     return data
 
 
