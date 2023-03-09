d7e5ead600f6cf777af3cec0a2cbca8ea87201b4
Joshua Rose <joshuarose099@gmail.com>
2023-03-10 10:25:28 +1100

    test(docs): added docstring headers

--- tests/test_unit_core.py
+++ tests/test_unit_core.py
@@ -1,17 +1,13 @@
 """
-The MIT License (MIT)
+source: Courier.courier
+test: tests.test_unit_core
+~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-Copyright (c) 2023 Joshua Rose
+This modules is reponsible for testing the
+primary Courier functions.
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+:copyright: (c) 2023 by Joshua Rose.
+:license: MIT, see LICENSE for more details.
 """
 
 import unittest
--- tests/test_unit_file_construct.py
+++ tests/test_unit_file_construct.py
@@ -1,21 +1,19 @@
 """
-The MIT License (MIT)
+test: tests.test_unit_file_constructor
+source: Courier.util.__init__
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-Copyright (c) 2023 Joshua Rose
+This module tests required dependencies are
+initialized and that global references and 
+such are set.
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+:copyright: (c) 2023 by Joshua Rose.
+:license: MIT, see LICENSE for more details.
 """
 
-import unittest
 import logging
+import unittest
+
 from util import display_if_online
 
 
